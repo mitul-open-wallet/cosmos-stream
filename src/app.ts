8ef52763c2553c952fbd6f7ea45c96da1b06e182
mitul-open-wallet <contact@openwallet.xyz>
2025-03-03 15:39:37 +0200

    Invoke cosmos monitor on demand

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express, {Express, Request, Response } from 'express'
 import { appConfig } from './config';
+import { CosmosWalletMonitor } from './controllers/CosmosWalletMonitor';
+
+const cosmosMonitor = new CosmosWalletMonitor("wss://cosmos-rpc.publicnode.com:443/websocket");
 
 const port = appConfig.port;
 const app: Express = express();
@@ -9,5 +12,6 @@ app.listen(port, () => {
 });
 
 app.get('/check', async (req: Request, response: Response) => {
+    await cosmosMonitor.bootstrap()
     response.status(200).send({"message": "hello"})
 });
