cf2ea891ed2a9dd0554ff02960d59dd490270cc3
min_max <mitul.manish@heathmont.net>
2025-03-03 23:32:34 +0200

    Run cosmos monitor on demand

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,13 @@ import { CosmosWalletMonitor } from './controllers/CosmosWalletMonitor';
 
 const port = appConfig.port;
 const app: Express = express();
+const cosmosMonitor = new CosmosWalletMonitor("wss://cosmos-rpc.publicnode.com:443/websocket");
 
 app.listen(port, () => {
     console.log(`server is running on: port ${port}`)
 });
 
 app.get('/check', async (req: Request, response: Response) => {
+    await cosmosMonitor.bootstrap()
     response.status(200).send({"message": "hello"})
 });
