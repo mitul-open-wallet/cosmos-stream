e727f26db648cf87bb051ad99f6d56b46b57f9fa
mitul-open-wallet <contact@openwallet.xyz>
2025-03-03 15:26:13 +0200

    Revert

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import express, {Express, Request, Response } from 'express'
 import { appConfig } from './config';
 
-// const port = appConfig.port;
-// const app: Express = express();
+const port = appConfig.port;
+const app: Express = express();
 
-// app.listen(port, () => {
-//     console.log(`server is running on: port ${port}`)
-// });
+app.listen(port, () => {
+    console.log(`server is running on: port ${port}`)
+});
 
-// app.get('/check', async (req: Request, response: Response) => {
-//     response.status(200).send({"message": "hello"})
-// });
+app.get('/check', async (req: Request, response: Response) => {
+    response.status(200).send({"message": "hello"})
+});
