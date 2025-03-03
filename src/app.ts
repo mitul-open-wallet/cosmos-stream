deaf328f5432c99dab97267480d3e6402db157aa
mitul-open-wallet <contact@openwallet.xyz>
2025-03-03 16:44:42 +0200

    Fix

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,6 @@ import express, {Express, Request, Response } from 'express'
 import { appConfig } from './config';
 import { CosmosWalletMonitor } from './controllers/CosmosWalletMonitor';
 
-const cosmosMonitor = new CosmosWalletMonitor("wss://cosmos-rpc.publicnode.com:443/websocket");
-
 const port = appConfig.port;
 const app: Express = express();
 
@@ -12,6 +10,5 @@ app.listen(port, () => {
 });
 
 app.get('/check', async (req: Request, response: Response) => {
-    await cosmosMonitor.bootstrap()
     response.status(200).send({"message": "hello"})
 });
