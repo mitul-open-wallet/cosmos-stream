e76e29005a48b432c25159f5b355950cc8cff9dc
mitul-open-wallet <contact@openwallet.xyz>
2025-03-05 15:22:39 +0200

    Fix

--- src/local.ts
+++ src/local.ts
@@ -1,8 +1,10 @@
 import { CosmosWalletMonitor } from "./controllers/CosmosWalletMonitor";
 
-async function main() {
+async function startObservingCosmosChain() {
     const cosmosMonitor = new CosmosWalletMonitor("wss://cosmos-rpc.publicnode.com:443/websocket");
     await cosmosMonitor.bootstrap()
 }
 
-main();
+setTimeout(async () => {
+    await startObservingCosmosChain()
+}, 50000)
